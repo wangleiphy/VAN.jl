8da7cb09a1096b1fbe3599cbe05b036081d34994
Lei Wang <wangleiphy@gmail.com>
2019-10-16 20:54:23 +0800

    improvment and added new test

--- src/loss.jl
+++ src/loss.jl
@@ -1,20 +1,19 @@
 using Flux.Optimise:update!
-export train, loss
 
-function get_energy(K::Array, samples)
+function get_energy(K::Matrix{T}, samples) where T <: Real
     energy = sum(samples .* (K*samples), dims=1)
 end
 
-function free_energy(K::Array, model::AutoRegressiveModel, samples)
+function free_energy(K::Matrix{T}, model::AutoRegressiveModel, samples) where T <: Real
     return mean(get_energy(K, samples) .+ get_logp(model, samples))
 end
 
-function loss(K::Array, model::AutoRegressiveModel, nbatch::Int)
+function loss(K::Matrix{T}, model::AutoRegressiveModel, nbatch::Int) where T <: Real
     samples = gen_samples(model, nbatch)
     free_energy(K, model, samples)
 end
 
-function loss_reinforce(K::Array, model::AutoRegressiveModel, samples)
+function loss_reinforce(K::Matrix{T}, model::AutoRegressiveModel, samples) where T <: Real
     e = get_energy(K, samples)
     logp = get_logp(model, samples)
     f = e .+ logp
@@ -22,12 +21,12 @@ function loss_reinforce(K::Array, model::AutoRegressiveModel, samples)
     return mean(logp.* (f .- b))
 end
 
-function grad_model(K::Array, model::AutoRegressiveModel, samples)
+function grad_model(K::Matrix{T}, model::AutoRegressiveModel, samples) where T <: Real
     model_grad = gradient(loss_reinforce, K, model, samples)[2]
     (model_grad.W..., model_grad.b...)
 end
 
-function train(K::Array, model::AutoRegressiveModel; optimizer=ADAM(0.1), nbatch::Int=100, niter::Int=100)
+function train(K::Matrix{T}, model::AutoRegressiveModel; optimizer=ADAM(0.1), nbatch::Int=100, niter::Int=100) where T <: Real
     θ = model_parameters(model)
     for i = 1:niter
         _, gθ, _ = gradient(loss, K, model, nbatch)
